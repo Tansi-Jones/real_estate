da4d5623d4505705cc6c31716199b7d333faf5fb
Tansi Jones <82033505+Tansi-Jones@users.noreply.github.com>
2025-03-18 23:14:44 +0200

    Add .gitignore, update package.json scripts, and improve code formatting
    - Added .gitignore to exclude node_modules
    - Introduced new scripts for development and server start in package.json
    - Refactored database connection and server files for consistency
    - Created example .env file for local development configuration
    - Improved formatting in SingleProperty component for better readability

--- src/components/pages/SingleProperty.jsx
+++ src/components/pages/SingleProperty.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import ImageCarousel from "../misc/ImageCarousel";
 import "./SingleProperty.css";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import properties from "../../datas/properties.js";
 import NotFound from "../pages/NotFound";
 
@@ -10,36 +10,33 @@ export default function SingleProperty() {
   const { id } = useParams();
 
   useEffect(() => {
-    setProperty(
-      properties.find((p) => p.id == id)
-    )
-    window.scrollTo(0, 0)
+    setProperty(properties.find((p) => p.id === id));
+    window.scrollTo(0, 0);
   }, [property, id]);
 
-  return (
-  property ?
+  return property ? (
     <section className="single-property">
-        <h1 className='page-heading'>{property.name}</h1>
-        <ImageCarousel images={property.images} />
-        <div className="info-button">
-          <div className="infos">
-            <div className="info1">
-              <h3>Target Profitability</h3>
-              <h3 className="cl-blue num">{property.profit}%</h3>
-            </div>
-            <div className="info2">
-              <h3>Objective of returned revenues</h3>
-              <h3 className="cl-blue num">{property.returns}%</h3>
-            </div>
-            <div className="info3">
-              <h3>Valuation of the property</h3>
-              <h3 className=" cl-blue num"> {property.price} ETH</h3>
-            </div>
+      <h1 className="page-heading">{property.name}</h1>
+      <ImageCarousel images={property.images} />
+      <div className="info-button">
+        <div className="infos">
+          <div className="info1">
+            <h3>Target Profitability</h3>
+            <h3 className="cl-blue num">{property.profit}%</h3>
           </div>
-          <button className="buy-button">Buy your NFT</button>
+          <div className="info2">
+            <h3>Objective of returned revenues</h3>
+            <h3 className="cl-blue num">{property.returns}%</h3>
+          </div>
+          <div className="info3">
+            <h3>Valuation of the property</h3>
+            <h3 className=" cl-blue num"> {property.price} ETH</h3>
+          </div>
         </div>
+        <button className="buy-button">Buy your NFT</button>
+      </div>
     </section>
-  :
-  <NotFound/>
+  ) : (
+    <NotFound />
   );
 }
